8200692dc58dbadb7e15098de5f2d778ee87e934
zwyhome <799378666@qq.com>
2017-01-09 23:48:28 +0800

    更新

---
+++ js/base.js
@@ -0,0 +1,10 @@
+/**
+ *百度统计
+ */
+var _hmt = _hmt || [];
+(function() {
+    var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?3931f56397aa3234b17897975af93205";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+})();
